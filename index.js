766a6054c0dd8e89b9239bda795e75e06e4d16a1
thomalex <thomas.gae@posteo.de>
2021-01-19 17:21:25 +0100

    Basic version plus ask functionality done!

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const random = (_) => {
   }
   if (renderColor !== null) {
     return renderColor;
-    // return chalk.hex(renderColor)(renderColor);
   } else return renderAsk;
 };
 
@@ -25,42 +24,34 @@ let markup = '';
 let color = 'green';
 const hexcode = random();
 let count = 0;
-
-for (let row = 0; row < 9; row++) {
-  if (row < 3 || row > 5) {
-    for (let col = 0; col < 31; col++) {
-      markup += '#';
-    }
-  } else if (row === 3 || row === 5) {
-    for (let col = 0; col < 31; col++) {
-      if (col < 3 || col > 27) {
+if (hexcode !== 'Type in a color') {
+  for (let row = 0; row < 9; row++) {
+    if (row < 3 || row > 5) {
+      for (let col = 0; col < 31; col++) {
         markup += '#';
-      } else {
-        markup += ' ';
+      }
+    } else if (row === 3 || row === 5) {
+      for (let col = 0; col < 31; col++) {
+        if (col < 3 || col > 27) {
+          markup += '#';
+        } else {
+          markup += ' ';
+        }
+      }
+    } else {
+      for (let col = 0; col < 31; col++) {
+        if (col < 3 || col > 27) {
+          markup += '#';
+        } else if (col < 12 || col > 18) {
+          markup += ' ';
+        } else if (col >= 12 && col <= 18) {
+          markup += hexcode[count];
+          count++;
+        }
       }
     }
-  } else {
-    for (let col = 0; col < 31; col++) {
-      if (col < 3 || col > 27) {
-        markup += '#';
-      } else if (col < 12 || col > 18) {
-        markup += ' ';
-      } else if (col >= 12 && col <= 18) {
-        markup += hexcode[count];
-        count++;
-      }
-    }
+    markup += '\n';
   }
-  markup += '\n';
-}
-
-//  for (let i=0; i<9; i++) {
-//    for (let x =0; x<32; x++) {
-//    if(x < 31) {
-//      markup += '#'
-//    } else markup += '\n'
-//  }
-//
-//  }
+} else console.log('Type in a color!');
 
 console.log(chalk.hex(hexcode)(markup));
